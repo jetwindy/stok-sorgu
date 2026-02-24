a449e13f5cf7e139bd79b8e2b8105572819d8a7a
jetwindy <ufuk@ufukaltintop.com>
2026-02-24 10:38:38 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 import streamlit as st
 import pandas as pd
 
-df = pd.read_excel("stok.xlsx", sheet_name="STOK")
+st.set_page_config(page_title="Stok Sistemi", layout="wide")
+
+# Google Sheets canlı bağlantı
+url = "https://docs.google.com/spreadsheets/d/189eP59G3ECgvXypR8UQ_BULqMSdZrLav/gviz/tq?tqx=out:csv&sheet=stok"
+
+df = pd.read_csv(url)
 df.columns = df.columns.str.strip()
 
 st.title("📦 Stok Sorgulama")
 
 secili_tel = st.selectbox("TEL Seç", sorted(df["TEL"].unique()))
-
 filtered_cins = df[df["TEL"] == secili_tel]["CİNS"].unique()
 secili_cins = st.selectbox("CİNS Seç", sorted(filtered_cins))
 
@@ -15,7 +19,6 @@ sonuc = df[(df["TEL"] == secili_tel) & (df["CİNS"] == secili_cins)]
 
 if not sonuc.empty:
     st.success("Sonuç bulundu ✅")
-
     st.dataframe(
         sonuc[[
             "RAF NO",
